c1ed448fb3bd3039599e40694b9bf9ca00cc5be2
LIUWEI <wei.liu@neulion.com.cn>
2018-05-17 10:58:30 +0800

    Add：TestIntentActivity added

---
+++ app/src/main/java/com/android/liuwei/myandroidcode/link/TestIntentActivity.java
@@ -0,0 +1,39 @@
+package com.android.liuwei.myandroidcode.link;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.android.liuwei.myandroidcode.BaseActivity;
+import com.android.liuwei.myandroidcode.R;
+
+import butterknife.OnClick;
+
+/**
+ * User: liuwei(wei.liu@neulion.com.com)
+ * Date: 2018-05-17
+ * Time: 10:11
+ */
+public class TestIntentActivity extends BaseActivity
+{
+    @Override
+    protected int getActivityLayout()
+    {
+        return R.layout.activity_test_intent;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @OnClick(R.id.intent_start_news)
+    public void startCntvAppNewsPage()
+    {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse("cntvlink://news"));
+        startActivity(intent);
+    }
+}
